4f7989b03c820737856cef021178a747c7afde35
Tutument <15735170682@163.com>
2020-08-28 09:05:04 +0800

    Screw提交

--- src/main/java/com/example/test/ThisTest/Test.java
+++ src/main/java/com/example/test/ThisTest/Test.java
@@ -2,6 +2,9 @@ package com.example.test.ThisTest;
 
 import org.springframework.util.Assert;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Test {
     public static void main(String[] args) {
         //对this指向的测试spuer()这个方法 在B中 this.super();
@@ -10,10 +13,21 @@ public class Test {
         A a = new A();
         a.get();*/
 
-        ass(null);
+        //ass(null);
+
+        ReentrantLock lock;
+
+        f();
 
     }
     public static void ass(String name){
         Assert.notNull(name, "Name must not be null!");
+    }
+
+    public static void f() {
+        String[] a = new String[2];
+        Object[] b = a;
+        a[0] = "hi";
+        b[1] = Integer.valueOf(42);
     }
 }
